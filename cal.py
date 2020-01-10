649997d2aabad136920f8d3c58220ad2c78b4e8f
Madhuri110 <55666869+Madhuri110@users.noreply.github.com>
2020-01-10 11:46:59 +0530

    calculator program code

---
+++ cal.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 16:15:12 2020
+
+@author: Vijay
+"""
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route('/')
+def calculator():
+    return render_template('calculator.html')
+
+@app.route('/result', methods=['POST' , 'GET'])
+def result():
+    num1=float(request.form["num1"])
+    num2=float(request.form["num2"])
+    
+    if request.method=="POST":
+        if request.form["process"]=="add":
+            return str(num1+num2)
+        if request.form["process"]=="substract":
+            return str(num1-num2)
+        if request.form["process"]=="multiply":
+            return str(num1*num2)
+        if request.form["process"]=="divide":
+            return str(num1/num2)
+        
+        
+        
+        
+if __name__=='__main__':
+    app.run()
